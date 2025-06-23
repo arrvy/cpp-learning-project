9cf25d2279efcf0c488749df0e29e92f95aa48e3
arrvy <nashr.wahyono@mhs.unsoed.ac.id>
2025-06-23 20:12:48 +0700

    up

---
+++ 2.python-basic/set.py
@@ -0,0 +1,21 @@
+set = {"firman","oky","faisal","eek","firman"}
+print(set)
+
+set.remove("firman")
+print(set)
+
+set.add("fajar") #pop doesnt have push/append/insert
+print(set)
+
+set.pop()
+print(set) #pop will removing random element
+
+boolean = {1,True,False,"+","-","*","/"}
+print(boolean)
+
+for x in set:
+    print(x)
+
+set3 = set.union(boolean)
+print(set3)
+
---
+++ 2.python-basic/unpack-tuples.py
@@ -0,0 +1,14 @@
+hewan = ("Ayam","Bebek","Sapi")
+x,y,z =hewan
+print(x,y,z)
+
+a,b,c = "Bubur","Nasi","Sayur"
+print(a,b,c)
+
+(a,b,c) = "Sempak","Baju","Celana"
+print(a,b,c)
+
+e,f,*g="Firman","Oky","Hanif","Faisal"
+print(e,f,g)
+#! Python are Unchangeable
+#hewan[1] = "aayam"
